eb7a69662b98644ff2e58a323bbb07e717d9e9e9
Vincent Spiewak <vspiewak@gmail.com>
2018-05-30 01:07:27 +0200

    Revert live commit

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -18,7 +18,6 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     return (
       <h1>
         <FormattedMessage {...messages.header} />
-        Salut le meetup ! (entreprise ready :)
       </h1>
     );
   }
